c1dac8a5db4881949de556eca98a572d29479cd7
Lene Signorelli <lenesignorelli@yahoo.com.br>
2023-03-21 16:08:22 -0300

    Cria componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [classificacao, setClassificacao] = useState([]);
+
+  useEffect(() => {
+    fetch('times.json')
+      .then(resp => resp.json())
+      .then(dados => {
+        const classificacaoInicial  = dados.map(time => {
+          return {nome: time.nome, sigla: time.sigla, pontos: 0, vitorias: 0, empates: 0,
+             derrotas: 0, golsPro: 0, golsContra: 0, saldoGols: 0};
+        });
+        setClassificacao(classificacaoInicial);
+      })
+      .catch(erro =>console.log(erro));    
+  }, [])
+  
   return (
     <div className="App">
       OLA
     </div>
+    
   );
 }
 
---
+++ src/components/Tabela/index.js
@@ -0,0 +1,8 @@
+
+export default function Tabela() {
+  return (
+    <div>
+        Pagina Tabela
+    </div>
+  )
+}
